6c535e40f62939108c43ddc79cab2c8a3945a708
jennytoc <toctocanj@gmail.com>
2022-06-21 23:27:51 -0600

    Added landing page

--- doggy-pile/src/App.js
+++ doggy-pile/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={ <LandingPage /> } />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
